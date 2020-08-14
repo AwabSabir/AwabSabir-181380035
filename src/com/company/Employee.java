5ce1a4c9455b76033f117dc28d083a3aa3b51188
Awab Sabir <181380035@gift.edu.pk>
2020-08-14 21:18:42 +0500

    created new abstract class

---
+++ src/com/company/Employee.java
@@ -0,0 +1,19 @@
+package com.company;
+
+abstract class Employee {
+    private int empid;
+    private  String name;
+    private  double taxtRate;
+
+  public  Employee(){
+      this.empid=0;
+      this.name="xyz";
+      this.taxtRate=0.1;
+
+  }
+  public Employee(int empid,String name, double taxtRate){
+
+  }
+  
+
+}
